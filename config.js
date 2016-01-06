1a44813559c9f6522c218da8d66dcc129132a447
Tyler Kellen <tyler@sleekcode.net>
2016-01-06 12:14:51 -0500

    fix deploy

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
   creds: isProduction ? require('../bots') : require('./bots'),
   isProduction: isProduction,
-  env: process.env.NODE_ENV ? 'production' : 'staging'
+  env: isProduction ? 'production' : 'staging'
 };
