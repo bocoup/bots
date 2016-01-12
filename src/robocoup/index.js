377765f702266b593fd2ff319baa8e48b82dbcca
Ben Alman <cowboy@rj3.net>
2016-01-12 10:18:50 -0500

    Cleanup robocoup code and allow it to handle changed messages.

--- src/robocoup/index.js
+++ src/robocoup/index.js
@@ -16,26 +16,38 @@ bot.on('open', function() {
 
 bot.on('message', function(message) {
   const channel = this.getChannelGroupOrDMByID(message.channel);
-  const user = this.getUserByID(message.user);
-  const subtype = message.subtype || 'normal';
-  if (message.type === 'message' && !message.subtype && channel.is_im) {
-    const input = deparse.call(this, message.text).split(' ');
-    const command = input[0];
-    const action = input.slice(1).join(' ');
-    const handler = commands[command] && commands[command].handler;
-    if (!handler) {
-      channel.send(`Unknown command \`${command}\`.`);
-      return;
-    }
-    Promise
-      .try(() => {
-        return handler(user, action === '' ? null : action);
-      })
-      .then(channel.send.bind(channel))
-      .catch(error => {
-        channel.send(`An unexpected error occurred: \`${error.message}\``);
-      });
+  // Ignore non-im messages or non-message messages.
+  if (!channel.is_im || message.type !== 'message') {
+    return;
   }
+  // If the message was a "changed" message, get the underlying message.
+  if (message.subtype === 'message_changed') {
+    message = message.message;
+  }
+  // Any message with a subtype can be safely ignored.
+  if (message.subtype) {
+    return;
+  }
+  // Parse command and args out of message.
+  const args = deparse(this, message.text).split(' ');
+  const command = args.shift();
+  const action = args.length > 0 ? args.join(' ') : null;
+  // Is there a handler registered for this command?
+  const handler = commands[command] && commands[command].handler;
+  if (!handler) {
+    channel.send(`Unknown command \`${command}\`.`);
+    return;
+  }
+  // Run the command!
+  Promise
+    .try(() => {
+      const user = this.getUserByID(message.user);
+      return handler(user, action);
+    })
+    .then(channel.send.bind(channel))
+    .catch(error => {
+      channel.send(`An unexpected error occurred: \`${error.message}\``);
+    });
 });
 
 export default bot;
