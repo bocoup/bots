840c6b2de00dbd992438ec0a3c20de6e27219db8
Tyler Kellen <tyler@sleekcode.net>
2016-01-20 13:59:54 -0500

    only send don't understand if the message is an im

--- src/thanksbot/index.js
+++ src/thanksbot/index.js
@@ -16,11 +16,11 @@ bot.on('message', function(message) {
   const general = this.getChannelByName('general');
   const user = this.getUserByID(message.user);
   const subtype = message.subtype || 'normal';
-  if (message.subtype) {
-    channel.send('Sorry, I don\'t understand '+message.subtype+' messages yet.');
-    return;
-  }
   if (message.type === 'message' && channel.is_im) {
+    if (message.subtype) {
+      channel.send('Sorry, I don\'t understand '+message.subtype+' messages yet.');
+      return;
+    }
     query('insert_thanks', user.name, deparse(this, message.text)).then(function () {
       channel.send('Thanks, your message has been recorded for next Monday :tada:');
       general.send('Someone just left a message!');
