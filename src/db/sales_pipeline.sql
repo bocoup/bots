8cedd9052b9b636435d88a177bcf7974ef56f2b5
Tyler Kellen <tyler@sleekcode.net>
2016-02-11 12:41:14 -0500

    add sales pipeline metric

---
+++ src/db/sales_pipeline.sql
@@ -0,0 +1,25 @@
+WITH weighted_deals AS (
+  SELECT
+    stage,
+    value,
+    CASE
+      WHEN stage = 1 THEN 0
+      WHEN stage = 2 THEN 0.01
+      WHEN stage = 3 THEN 0.05
+      WHEN stage = 4 THEN 0.15
+      WHEN stage = 5 THEN 0.40
+      WHEN stage = 6 THEN 0.60
+      WHEN stage = 7 THEN 0.90
+    END AS weight
+  FROM deal
+  WHERE status = 'open'
+  AND DATE(created_at) = CURRENT_DATE
+  AND pipeline_id = 1
+)
+SELECT
+  COUNT(*) AS deals,
+  wd.stage AS stage,
+  ROUND(SUM(wd.value * wd.weight)) AS value
+FROM weighted_deals AS wd
+GROUP BY stage
+ORDER BY stage;
---
+++ src/robocoup/commands/pipeline.js
@@ -0,0 +1,52 @@
+/*
+ * A command to show the utilization status of the company today.
+ */
+import R from 'ramda'
+import moment from 'moment';
+
+import {query} from '../../lib/db';
+
+export const usage =
+  'Show information about our sales pipeline:\n`Usage: pipeline`';
+
+const currency = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 0,
+});
+
+const targets = {
+  "2016": [2500000,2500000,2500000,2500000]
+};
+
+// todo add a sync for this to the integrations repo
+const stages = [
+  'Stalled',
+  'Initial Outbound',
+  'Initial Contact',
+  'New Deal',
+  'Defining Scope',
+  'Defining Terms',
+  'Procurement'
+];
+
+export function handler (meta) {
+  return query('sales_pipeline')
+    .then((results) => {
+      const year = moment().format('YYYY');
+      const quarter = moment().format('Q');
+      const target = targets[year][parseInt(quarter)];
+      const actual = R.sum(R.map(parseInt)(R.pluck('value')(results)));
+      const differential = Math.round(actual/target*100);
+      return [
+        `> *Sales Pipeline Status as of ${moment().format('MMMM Do, YYYY')}*`,
+        `> The weighted value of our sales pipeline is currently ​*${currency.format(actual)}​*.`,
+        `> We are at *${differential}%* of our ongoing goal of *${currency.format(target)}* for *Q${quarter}* of *${year}*.`,
+        `>`
+      ].concat(results.map((metric) => {
+        const stage = stages[parseInt(metric.stage)-1]
+        const dealText = metric.deals === 1 ? 'deal' : 'deals';
+        return `> We have *${metric.deals}* ${dealText} in *${stage}* for a weighted value of *${currency.format(metric.value)}*.`
+      }));
+    });
+}
