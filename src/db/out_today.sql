c2a6fae8cfd01e50c43843c2af099389aa2bbc13
Tyler Kellen <tyler@sleekcode.net>
2016-02-12 14:15:29 -0500

    fix out today query

--- src/db/out_today.sql
+++ src/db/out_today.sql
@@ -3,6 +3,6 @@ FROM utilization u
 INNER JOIN employee e ON e.id=u.employee_id
 WHERE (
   u.leave_request_type_id IS NOT null AND
-  u.sketch_calendar_is IS null AND
+  u.sketch_calendar_id IS null AND
   CURRENT_DATE BETWEEN u.first_day AND u.last_day
 );
