b12f2436d0b5f91b56875fd9f686486cb43f04fe
Tyler Kellen <tyler@sleekcode.net>
2016-02-29 09:47:06 -0500

    move goal for utilization metric

--- src/robocoup/commands/utilization.js
+++ src/robocoup/commands/utilization.js
@@ -15,10 +15,10 @@ export function handler (meta) {
     .then((metric) => {
       return [
         `> *Utilization Status as of ${moment().format('MMMM Do, YYYY')}*`,
-        `> Our utilization goal is *80%* of our *${metric.billable_count}* billable team members on billable B2B projects.`,
         `> During the *last 30 days* our utilization rate was *${metric.last_30_days}%*.`,
         `> During the *next 30 days* our utilization rate is currently scheduled to be *${metric.next_30_days}%*.`,
         `> Year to date, our utilization rate has been *${metric.ytd}%*.`,
+        `> Our utilization goal is *80%* of our *${metric.billable_count}* billable team members on billable B2B projects.`
       ];
     });
 }
