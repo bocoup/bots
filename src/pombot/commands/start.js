a526e0ce9e399fe88588aa9e0e39a2b3b074eff9
Jenn Schiffer <jenn@dotbiz.info>
2016-05-24 12:40:54 -0400

    start command conditional logic
    export states, fake timer
    
    add pom class
    
    use pom class in start and stop commands
    
    introduce timer object, pass pom to conversation
    
    add time elapsed tracking to timer
    
    refactor createmessagehandler
    
    move timer logic into pom
    :
    
    refactor pom and index to send responses
    
    get sendresponse to work
    
    get pom working in channel
    
    refactor states

--- src/pombot/commands/start.js
+++ src/pombot/commands/start.js
@@ -1,11 +1,27 @@
 /*
- * A stub for the "start" command.
+ * The `start` command, which starts a Pom.
  */
 import {createCommand} from 'chatter';
+import states from '../states';
 
-export default createCommand({
-  name: 'start',
-  description: 'Starts a pom timer.',
-}, () => {
-  return `> Theoretically I should be starting a pom right now.`;
-});
+export default function(pom) {
+  return createCommand({
+    name: 'start',
+    description: 'Starts a pom timer.',
+  }, message => {
+
+    switch (pom.state) {
+      case states.RUNNING:
+        return `there is already a pom running, there are *${pom.getMinutes(pom.timeLeft)} minutes* left.`;
+        break;
+      case states.ON_BREAK:
+        return `we are in the middle of a break, there are *${pom.getMinutes(pom.timeLeft)} minutes* left.`;
+        break;
+      default:
+        // if pom is not running, start a new one
+        pom.start();
+        return `pom started; you have *${pom.getMinutes(pom.maxSeconds)} minutes*!`;
+        break;
+    }
+  });
+}
