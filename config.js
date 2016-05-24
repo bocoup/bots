75847cda0d26f410368bb58d1bed2f7dcec4be34
Jenn Schiffer <jenn@dotbiz.info>
2016-05-24 11:08:44 -0400

    fix trailing comma missing

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   email: {
     key: process.env.AWS_ACCESS_KEY_ID,
     secret: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_DEFAULT_REGION || 'us-east-1'
+    region: process.env.AWS_DEFAULT_REGION || 'us-east-1',
   },
   db: {
     // these defaults assume an ssh tunnel to our staging database.
