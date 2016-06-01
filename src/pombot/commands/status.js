e2be43179d3c0052de32e9ed9c1f1a2f4169c241
Ben Alman <cowboy@rj3.net>
2016-06-01 11:56:57 -0400

    Update pombot to use chatter 0.3.
    * Add tomato icon.
    * Enable logging of stack traces on exceptions.
    * Add "getCommand" command name formatting helper function.
    * Convert sub-command factories into commands.
    * Use createArgsAdjuster to pass pom instance and name formatting helper
      function into commands instead of using factories and closures.
    * Reduce repetition in SlackBot createMessageHandler code.
    * Use heredoc-tag lib to format a few lines that would have been > 120
      chars.

--- src/pombot/commands/status.js
+++ src/pombot/commands/status.js
@@ -1,34 +1,39 @@
 /*
  * The `status` command, which describes the status of a current Pom.
  */
+import heredoc from 'heredoc-tag';
 import {createCommand} from 'chatter';
 import {states} from '../pomConfig';
 
-export default function(pom) {
-  return createCommand({
-    name: 'status',
-    description: 'Displays the status of the current pom.',
-  }, (message, {channel}) => {
+export default createCommand({
+  name: 'status',
+  description: 'Displays the status of the current pom.',
+}, (message, {channel, pom, getCommand}) => {
 
-    // retrieve any tasks in this pom.
-    const taskList = [];
-    for (const user in pom.taskCollection) {
-      taskList.push(`> ${user} will ${pom.taskCollection[user]}`);
-    }
+  // retrieve any tasks in this pom.
+  const taskList = [];
+  for (const user in pom.taskCollection) {
+    taskList.push(`> ${user} will ${pom.taskCollection[user]}`);
+  }
 
-    const taskHeader = (taskList.length > 0) ? 'here is the task list:' : 'there are no tasks declared.';
+  const taskHeader = (taskList.length > 0) ? 'here is the task list:' : 'there are no tasks declared.';
 
-    switch (pom.state) {
-      case states.RUNNING:
-      case states.ON_BREAK:
-        // if pom is running, get time left
-        const timeLeft = pom.getTimeString(pom.timeLeft);
-        return [`a pom is currently running with *${timeLeft}* left and ${taskHeader}`, taskList];
-        break;
-      default:
-        // if pom is not running
-        return [`there is no pom currently running – start one with the command \`start\`. ${taskHeader}`, taskList];
-        break;
-    }
-  });
-}
+  switch (pom.state) {
+    case states.RUNNING:
+    case states.ON_BREAK:
+      // if pom is running, get time left
+      const timeLeft = pom.getTimeString(pom.timeLeft);
+      return [`a pom is currently running with *${timeLeft}* left and ${taskHeader}`, taskList];
+      break;
+    default:
+      // if pom is not running
+      return [
+        heredoc.oneline.trim`
+          there is no pom currently running – start one with the command \`${getCommand('start')}\`.
+          ${taskHeader}
+        `,
+        taskList,
+      ];
+      break;
+  }
+});
