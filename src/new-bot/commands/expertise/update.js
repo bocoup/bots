728ad288531af9b63f79921826433048268996cf
Ben Alman <cowboy@rj3.net>
2016-06-03 16:48:48 -0400

    Add "expertise update ___" dialog support. Closes #137.

--- src/new-bot/commands/expertise/update.js
+++ src/new-bot/commands/expertise/update.js
@@ -1,7 +1,10 @@
 import Promise from 'bluebird';
+import moment from 'moment';
+import heredoc from 'heredoc-tag';
 import {createCommand, createMatcher, createParser} from 'chatter';
 import {query} from '../../../lib/db';
-import {findExpertiseAndHandleErrors, abort} from './lib/query';
+import {findExpertiseAndHandleErrors, getIntExpScales, abort} from './lib/query';
+import {questions} from './lib/dialog';
 
 const intExpProps = ['interest', 'experience'];
 
@@ -36,16 +39,191 @@ function updateExpertise({userName, expertise, newValues}) {
   });
 }
 
-const updateMissingHandler = createMatcher({match: 'missing'}, (_, {user}) => {
-  return `> update missing for ${user.name} (coming soon)`;
-});
+// =================================
+// expertise update <expertise name>
+// =================================
+
+function updateExpertiseQuestions(state, header) {
+  const {
+    scales,
+    oldValues,
+    newValues,
+    userName,
+    expertise,
+    expertiseName,
+    command,
+    skippable,
+    done,
+  } = state;
+
+  const startOver = () => updateExpertiseQuestions(state, '_Starting over._');
+
+  const hasInterestChanged = () => !oldValues || newValues.interest !== oldValues.interest;
+  const hasExperienceChanged = () => !oldValues || newValues.experience !== oldValues.experience;
+
+  function getInterestQuestion() {
+    return {
+      question: `Choose your interest level for ${expertiseName}:`,
+      choices: scales.interest,
+      onAnswer: answer => {
+        newValues.interest = Number(answer);
+        return `_You selected *${newValues.interest}* for interest, thanks!_`;
+      },
+    };
+  }
+
+  function getExperienceQuestion() {
+    return {
+      question: `Choose your experience level for ${expertiseName}:`,
+      choices: scales.experience,
+      onAnswer: answer => {
+        newValues.experience = Number(answer);
+        return `_You selected *${newValues.experience}* for experience, thanks!_`;
+      },
+    };
+  }
+
+  function getConfirmNoChangeQuestion() {
+    return {
+      question: heredoc.trim.unindent`
+        Your interest and experience for ${expertiseName} haven't changed! Is this ok?
+        > Interest: *${scales.interest[newValues.interest]}*
+        > Experience: *${scales.experience[newValues.experience]}*
+
+      `,
+      choices: [
+        `Continue without making changes.`,
+        `No, re-choose interest and experience for ${expertiseName}.`,
+      ],
+      onAnswer(answer) {
+        if (answer === 2) {
+          return startOver();
+        }
+        return done(`_Expertise for *${expertise.expertise}* unchanged._`);
+      },
+    };
+  }
+
+  function getReasonQuestion() {
+    if (oldValues) {
+      const parts = [];
+      if (hasInterestChanged()) { parts.push('interest'); }
+      if (hasExperienceChanged()) { parts.push('experience'); }
+      const str1 = parts.length === 1 ? 'has' : 'have';
+      const str2 = parts.join(' and ');
+      return {
+        question: `Why ${str1} your ${str2} changed for ${expertiseName}?`,
+        onAnswer: answer => {
+          newValues.reason = answer;
+          return '_Noted!_';
+        },
+      };
+    }
+  }
+
+  function getConfirmChangeQuestion() {
+    const reason = 'reason' in newValues ? `> Reason: *${newValues.reason}*\n` : '';
+    return {
+      question: heredoc.trim.unindent`
+        You've entered the following for ${expertiseName}. Is this ok?
+        > Interest: *${scales.interest[newValues.interest]}*
+        > Experience: *${scales.experience[newValues.experience]}*
+        ${reason}
+      `,
+      choices: [
+        `Save these changes.`,
+        `No, re-choose interest and experience for ${expertiseName}.`,
+      ],
+      onAnswer(answer) {
+        if (answer === 2) {
+          return startOver();
+        }
+        return updateExpertise({userName, expertise, newValues}).then(done);
+      },
+    };
+  }
+
+  return questions({
+    header,
+    questions: [
+      () => getInterestQuestion(),
+      () => getExperienceQuestion(),
+      () => {
+        if (!hasInterestChanged() && !hasExperienceChanged()) {
+          return getConfirmNoChangeQuestion();
+        }
+        return [
+          () => getReasonQuestion(),
+          () => getConfirmChangeQuestion(),
+        ];
+      },
+    ],
+    strExit: skippable ? ['exit', 'skip'] : ['exit'],
+    fmtPrompt([exit, skip]) {
+      const str1 = skip ? `, type *${skip}* to skip, or` : ' or type';
+      return `Please answer${str1} *${exit}* to cancel.`;
+    },
+    onExit(exit) {
+      if (exit === 'skip') {
+        return done(`_Skipping ${expertiseName} for now!_`, true);
+      }
+      return `Canceled, please type \`${command}\` to try again.`;
+    },
+  });
+}
+
+function updateExpertiseDialog({
+  userName,
+  expertise,
+  command,
+  oneTimeHeader = null,
+  skippable = false,
+  done,
+}) {
+  return Promise.all([
+    getIntExpScales(),
+    query('expertise_by_bocouper_id', userName, expertise.id),
+  ])
+  .spread((scales, [oldValues]) => {
+    const expertiseName = `*${expertise.expertise}*`;
+    const newValues = {};
+
+    let lastUpdated;
+    if (oldValues) {
+      const formatted = moment.duration(-oldValues.seconds_since_last_update, 'seconds').humanize(true);
+      lastUpdated = `_You last updated this expertise *${formatted}*._`;
+    }
+
+    const state = {
+      scales,
+      oldValues,
+      newValues,
+      userName,
+      expertise,
+      expertiseName,
+      command,
+      skippable,
+      done,
+    };
+
+    return updateExpertiseQuestions(state, [
+      oneTimeHeader,
+      lastUpdated,
+      '',
+      `> ${expertiseName} / *${expertise.area}* / *${expertise.type}*`,
+      expertise.description && `${expertise.description.replace(/^/gm, '> ')}`,
+    ]);
+  });
+}
 
 export default createCommand({
   name: 'update',
   description: 'Update your interest and experience for the given expertise.',
   usage: '[missing | <expertise name> [interest=<1-5> experience=<1-5>]]',
 }, [
-  updateMissingHandler,
+  createMatcher({match: 'missing'}, (_, {user}) => {
+    return `> update missing for ${user.name} (coming soon)`;
+  }),
   createParser({
     parseOptions: {
       experience: Number,
@@ -75,7 +253,14 @@ export default createCommand({
       else if (numProps === 2) {
         return updateExpertise({userName, expertise, newValues}).then(done);
       }
-      return `> update ${search} for ${userName} (coming soon)`;
+      const command = `expertise update ${search.toLowerCase()}`;
+      return updateExpertiseDialog({
+        userName,
+        expertise,
+        command,
+        oneTimeHeader: output.splice(0, output.length),
+        done,
+      });
     })
     // Error! Print all cached output + error message + usage info, or re-throw.
     .catch(error => {
