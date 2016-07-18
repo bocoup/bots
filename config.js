8360ff79013692679b282240b799a7efaea2b05a
Ben Alman <cowboy@rj3.net>
2016-07-18 12:26:49 -0400

    Update secrets fetching / var names.

--- config.js
+++ config.js
@@ -11,15 +11,13 @@ module.exports = {
   email: {
     key: process.env.AWS_ACCESS_KEY_ID,
     secret: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_DEFAULT_REGION || 'us-east-1',
+    region: process.env.AWS_DEFAULT_REGION,
   },
   db: {
-    // these defaults assume an ssh tunnel to our staging database.
-    // see the npm script 'tunnel'
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || 'bocoup',
-    port: process.env.DB_PORT || 5400,
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGNAME,
+    port: process.env.PGPORT,
   },
 };
