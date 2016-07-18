03f84b0a0bd4a0f58a1a90dea5e380100564c9b2
Ben Alman <cowboy@rj3.net>
2016-07-18 14:46:08 -0400

    Simplify AWS SES code / config.

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ module.exports = {
     thanksbot: process.env.TOKEN_THANKSBOT
   },
   email: {
-    key: process.env.AWS_ACCESS_KEY_ID,
-    secret: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_DEFAULT_REGION,
   },
   db: {
