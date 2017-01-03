f98aa08cff22c0630c5289c2de7ce1a3503cf059
Tyler Kellen <tyler@sleekcode.net>
2017-01-03 10:03:05 -0500

    retreive utilization type and initiative from project phase

--- src/db/status.sql
+++ src/db/status.sql
@@ -8,10 +8,11 @@ SELECT
 FROM utilization AS u
 INNER JOIN employee AS e ON e.id=u.employee_id
 INNER JOIN employee AS check_in_person ON check_in_person.id=e.supporter_id
-INNER JOIN utilization_type AS ut ON ut.id=u.utilization_type_id
 LEFT JOIN leave_request_type AS lrt ON lrt.id=u.leave_request_type_id
-INNER JOIN initiative AS i ON i.id=u.initiative_id
 LEFT JOIN project AS p ON p.id=u.project_id
+LEFT JOIN project_phase AS pp ON pp.id=u.project_phase_id
+LEFT JOIN utilization_type AS ut ON ut.id=COALESCE(pp.utilization_type_id, u.utilization_type_id)
+LEFT JOIN initiative AS i ON i.id=COALESCE(pp.initiative_id, u.initiative_id)
 WHERE (
   CURRENT_DATE BETWEEN u.first_day AND u.last_day AND
   u.sketch_calendar_id IS null AND
