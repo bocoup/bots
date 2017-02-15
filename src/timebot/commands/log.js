d043ba78d3d6a76eb691332461031d1c8bcc14fc
Corey Frang <gnarf@gnarf.net>
2017-02-15 13:45:01 -0500

    Remove undo functionality for now

--- src/timebot/commands/log.js
+++ src/timebot/commands/log.js
@@ -7,43 +7,6 @@ import {query} from '../../lib/db';
 
 import {createCommand} from 'chatter';
 
-function confirm({inserted: {id, duration} = {}, project, description, user, bot}) {
-  const formatted = moment.duration(duration).format('h[h]mm[m]');
-  const logMessage = `:timer_clock:${formatted} ${user.name} [${project}] ${description}`;
-  let deleted = false;
-  let announced = false;
-  const channelName = 'continued-access';
-  const channel = bot.slack.rtmClient.dataStore.getChannelByName(channelName).id;
-
-  if (!id) {
-    return `Error finding *${project}* as a project (or your slack username), try saying *projects* for a list.`;
-  }
-
-  setTimeout(() => {
-    if (!deleted) {
-      bot.sendResponse({channel}, logMessage);
-      announced = true;
-    }
-  }, 30000);
-
-  return {
-    message: `This log entry has been stored, you can reply with *undo* if you want to delete it (typo, or any other reason)
-${logMessage}`,
-    dialog(message) {
-      if (message.toLowerCase() === 'undo') {
-        deleted = true;
-        return query('delete_log', id)
-          .then(() => {
-            if (announced) {
-              bot.sendResponse({channel}, `${user.name} undid that entry`);
-            }
-            return 'Deleted the last log';
-          });
-      }
-    },
-  };
-}
-
 export default createCommand({
   name: 'log',
   description: 'Create a timelog entry',
@@ -59,7 +22,20 @@ export default createCommand({
   }
 
   return query('insert_log', interval, description, project.toLowerCase(), user.name)
-    .then(([inserted]) =>
-      confirm({project, description, user, inserted, bot})
-    );
+    .then(([inserted]) => {
+      if (!inserted) {
+        return `Error finding *${project}* as a project (or your slack username), try saying *projects* for a list.`;
+      }
+
+      const {id, duration} = inserted
+      const formatted = moment.duration(duration).format('h[h]mm[m]');
+      const logMessage = `:timer_clock:${formatted} ${user.name} [${project}] ${description}`;
+      const channelName = 'continued-access';
+      const channel = bot.slack.rtmClient.dataStore.getChannelByName(channelName).id;
+
+      bot.sendResponse({channel}, logMessage);
+
+      return `This log entry has been stored.
+    ${logMessage}`;
+    });
 });
