30c8fa7800cbb2170ace3c8fd198853847464f5b
Corey Frang <gnarf@gnarf.net>
2017-02-22 17:16:46 -0500

    Weekly report

--- src/timebot/commands/index.js
+++ src/timebot/commands/index.js
@@ -1,5 +1,4 @@
 export {default as log} from './log';
 export {default as projects} from './projects';
 export {default as project} from './project';
-export {default as last} from './last';
 export {default as weekly} from './weekly';
