48b98ec8a88829208484de9376aa835324eb4ee5
Amal Hussein <amalhussein08@gmail.com>
2019-03-15 15:14:19 -0400

    moved resolve

--- src/robocoup/commands/comm-prefs.js
+++ src/robocoup/commands/comm-prefs.js
@@ -33,8 +33,8 @@ export default createCommand({
         ---------------------------------------------------------------
 `;
       });
-  });
-    resolve(answer);
+      resolve(answer);
+    });
   }).catch((error) => {
     reject(`whooooops something went wrong ${error}`);
   });
